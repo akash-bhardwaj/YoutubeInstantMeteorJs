5209c047bbd81ef1fccf848786a976896fa81455
Akash Bhardwaj <akashbdj@gmail.com>
2015-01-19 23:56:16 +0530

    suggestion and instant logic

--- client/index.js
+++ client/index.js
@@ -7,8 +7,65 @@ Template.loadPlayer.rendered = function(){
 
 }
 	
-function onYouTubePlayerReady(playerId) {
+onYouTubePlayerReady = function(playerId) {
 	ytplayer = document.getElementById("myytplayer");
 	ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
 }
 
+Template.searchBox.events({
+	'keyup' : function(e,t){
+		doSearch();
+	}
+});
+
+doSearch = function(){
+	var toSearch = $("#searchbox");
+		toSearch.autocomplete({
+	    source: function(request, response) {
+	      $.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
+	          {
+	            "hl":"en", 
+	            "ds":"yt",
+	            "jsonp":"suggestCallBack", 
+	            "q":request.term, 
+	            "client":"youtube"
+	          }
+	      );
+
+	      suggestCallBack = function (data) {
+	        var suggestions = [];
+	        $.each(data[1], function(key, val) {
+	            suggestions.push({"value":val[0]});
+	        });
+	        suggestions.length = 5;
+	        response(suggestions);
+	      };
+
+	      this.liveRegion = $( "<span>", {
+	        role: "status",
+	              "aria-live": "polite"
+	      }).addClass("ui-helper-hidden-accessible");
+	    }
+  	});
+  	var yt_url= 'http://gdata.youtube.com/feeds/api/videos?q='+encodeURIComponent(toSearch.val())+'&format=5&max-results=1&v=2&alt=jsonc';
+  	$.ajax({
+	    type: "GET",
+	    url: yt_url,
+	    dataType:"jsonp",
+	    success: function(response){
+	      if(response.data.items){
+	        videos = response.data.items;
+	        var playList = [];
+	        playList.push(videos);
+	        $.each(videos, function(i, data){
+	          var video_id = data.id;
+	          var video_title = data.title;
+	          if(ytplayer){
+	            ytplayer.loadVideoById(video_id);
+	          }
+	          document.getElementById("video-title").innerHTML = video_title;
+	        });
+	      }
+	    }
+	  }); 
+}
