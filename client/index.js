21e66f804cb37e839ef889d7c8153cc88ac5f2fb
Akash Bhardwaj <akashbdj@gmail.com>
2015-01-21 02:21:21 +0530

    some css

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 Template.loadPlayer.rendered = function(){
 	var VIDEO_ID = "juRUOGv1T2k";
-	var params = { allowScriptAccess: "always" };
+	var params = { allowScriptAccess: "always", allowFullScreen: true };
 	var atts = { id: "myytplayer" };
 	swfobject.embedSWF("http://www.youtube.com/v/" + VIDEO_ID + "?enablejsapi=1&playerapiid=ytplayer&version=3&rel=0&autoplay=0&autohide=1&egm=0&loop=0&fs=1&hd=0&showsearch=0&showinfo=0&iv_load_policy=3&cc_load_policy=1",
-	                 "ytapiplayer", "781", "422", "8", null, null, params, atts);
+	                 "ytapiplayer", "754", "432", "8", null, null, params, atts);
 
 }
 
@@ -19,7 +19,7 @@ Template.playLists.helpers({
 	 }
 });
 Template.playLists.events({
-	'click .each-video' : function(e){
+	'click #each-video' : function(e){
 		e.preventDefault();
 		var vidId = e.currentTarget.dataset.id;
 		var vidTitle = e.currentTarget.dataset.title;
